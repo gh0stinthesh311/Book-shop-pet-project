f020a8a4dbde38a8310e1fd67816954d77882f29
Bagzhan Sadvakassov <sadvakasov.bagjan@gmail.com>
2024-06-28 15:56:40 +0500

    its now books

--- src/main/java/io/github/soydivision/book/BookController.java
+++ src/main/java/io/github/soydivision/book/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/book")
+@RequestMapping("/api/books")
 public class BookController
 {
     @Autowired
